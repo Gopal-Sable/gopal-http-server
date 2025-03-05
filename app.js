97423775fc09d29989d2219ab9c95832d6aa1bdc
Gopal Sable <sablegopal123@gmail.com>
2025-03-05 11:20:12 +0530

    created first route

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const { error } = require("console");
 const http = require("http");
+const fs = require("fs/promises");
+
 const port = 3000;
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/html":
+      getHtml(res);
+      break;
+
+    default:
+      break;
+  }
+});
 
 server.listen(port, (error) => {
   if (error) {
@@ -11,3 +21,18 @@ server.listen(port, (error) => {
     console.log("Server running on port", port);
   }
 });
+
+function getHtml(res) {
+  fs.readFile("index.html")
+    .then((data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      return res.write(data);
+    })
+    .catch((err) => {
+      res.writeHead(404);
+      res.write("Page not found");
+    })
+    .finally(() => {
+      res.end();
+    });
+}
